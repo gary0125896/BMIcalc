18c6aca1a45c513fd4e70672d230204ee91ec462
gary0125896 <ilovegumiand@gmail.com>
2023-10-07 13:47:14 +0800

    git commit

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ weight = int(input("Please input your weight:"))
 bmi = weight / (height / 100) ** 2
 print("your height:"+str(height))
 print("your weight:"+str(weight))
-print("your bmi:"+str(round(bmi, 2)))
+print("your bmi:"+str(round(bmi, 2)))#123
